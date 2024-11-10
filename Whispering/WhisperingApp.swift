6843ae6d9e841805ddf6cd06aeb11c92054a8af4
Rudrank Riyam <rudrankriyam@gmail.com>
2024-11-10 16:07:16 +0530

    Add Whsipering project with WhisperKit

---
+++ Whispering/WhisperingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperingApp.swift
+//  Whispering
+//
+//  Created by Rudrank Riyam on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WhisperingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
